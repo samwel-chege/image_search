641fe39d0ab31472ebcdce7f3e0baa209cf6f34c
samwel-chege <samwelchegeh09@gmail.com>
2022-04-12 22:53:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-function App() {
+
+
+const Access_Key = process.env.Access_Key
+const App = () => {
+
+  const [img,setImg] = useState("");
+  const[res, setRes] = useState([]);
+
+
+  //make the api request
+
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=l_UvzAGpCXL07RkJX0Pfx9kQrYh3BtbqlsrjnaDSbSA&per_page=20`
+    );
+
+  
+
+  const dataJ = await data.json();
+  const result = dataJ.results;
+  console.log(result);
+  setRes(result);
+  };
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
+
+
+  const Submit = () => {
+    fetchRequest();
+    setImg("");
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <br />
+      
+      <div className="container-fluid">
+        <div className="row">
+        
+          <div className="col-12 d-flex justify-content-center align-items-center input">
+            <input
+              className="col-3 form-control-sm py-1 fs-4 text-capitalize border border-3 border-dark"
+              type="text"
+              placeholder="Search Anything..."
+              value= {img}
+              onChange={(e) => setImg(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={Submit}
+              className="btn bg-dark text-white fs-3 mx-3"
+            >
+              Search
+            </button>
+          </div>
+        </div>
 
+            <br />
+          <div className="col-12  d-flex justify-content-evenly flex-wrap">
+            {res.map((val) => {
+              return (
+                <>
+                  <img
+                    className="col-3 shadow-1-strong rounded mb-4 img-fluid img-thumbnail"
+                    src={val.urls.small}
+                    alt="val.alt_description"
+                  />
+                </>
+              );
+            })}
+          </div>
+     
+        
+      
+      </div>
+      <br />
+      {/* <section className="overflow-hidden text-gray">
+        <div className="container  mx-auto ">
+          <div className="flex flex-wrap ">
+            <div className="flex flex-wrap ">
+              <div className="" >
+              {res.map((val) => {
+                return (
+                  <>
+                    <img
+                      className="block object-cover object-center rounded-lg"
+                      src={val.urls.small}
+                      alt="val.alt_description"
+                    />
+                  </>
+                );
+              })}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section> */}
+
+      {/* <div className="row">
+      <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        {res.map((val) => {
+          return (
+            <>
+              <img
+                className="w-100 shadow-1-strong rounded mb-4"
+                src={val.urls.small}
+                alt="val.alt_description"
+              />
+            </>
+          );
+        })}
+      </div>
+      </div>; */}
+
+      
+    </>
+  );
+};
 export default App;
